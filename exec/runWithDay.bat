3169c8cf812d55d2ac246edaa440a470e3275965
fengcheng <lufengcheng@bdfint.com>
2021-02-19 16:36:01 +0800

    Initial commit

--- exec/runWithDay.bat
+++ exec/runWithDay.bat
@@ -1,4 +1,4 @@
 @echo off
 title handleExcel
 set path=%cd%\jre\bin
-%path%\java.exe -jar handleExcel.jar ./src.xlsx ./result.xlsx ./process.xlsx
+%path%\java.exe -jar handleExcel.jar ./src.xlsx ./result.xlsx ./process.xlsx d
